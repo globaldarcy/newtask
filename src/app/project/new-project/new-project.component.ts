d89d1ea193b97abda4a3357e106900312cb8cc42
Darcy <289904316@qq.com>
2018-01-17 00:02:51 +0800

    test

---
+++ src/app/project/new-project/new-project.component.ts
@@ -0,0 +1,26 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { OverlayContainer } from '@angular/cdk/overlay';
+
+@Component({
+  selector: 'app-new-project',
+  templateUrl: './new-project.component.html',
+  styleUrls: ['./new-project.component.scss']
+})
+export class NewProjectComponent implements OnInit {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: any,
+    private dialogRef: MatDialogRef<NewProjectComponent>,
+    private oc: OverlayContainer
+  ) { }
+
+  ngOnInit() {
+    console.log(JSON.stringify(this.data));
+    this.oc.getContainerElement().classList.add(this.data.dark ? 'myapp-dark-theme' : null);
+  }
+
+  onClick() {
+    this.dialogRef.close('I received your message');
+  }
+}
---
+++ src/app/project/project-list/project-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from "@angular/material";
+import { NewProjectComponent } from "../new-project/new-project.component";
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['./project-list.component.scss']
+})
+export class ProjectListComponent implements OnInit {
+
+  public projects = [
+    {
+      name: "某某公司 ERP 系统",
+      desc: "为某某公司开发的定制化 ERP 系统",
+      coverImg: "/assets/img/covers/20.jpg"
+    },
+    {
+      name: "某某公司 ERP 系统",
+      desc: "为某某公司开发的定制化 ERP 系统",
+      coverImg: "/assets/img/covers/1.jpg"
+    }
+  ];
+  constructor(private dialog: MatDialog) { }
+
+  ngOnInit() {
+  }
+
+  openNewProjectDialog() {
+    const dialogRef = this.dialog.open(NewProjectComponent, {data: {dark: true}});
+    dialogRef.afterClosed().subscribe(result => console.log(result));
+  }
+}
